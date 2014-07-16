b6ea98b6a52ff0de226187ea9deb3f2180b3bff8
Sean Walsh <swalsh@viridityenergy.com>
2014-07-16 17:21:48 -0400

    Removed stateful module supervisor in favor of locator pattern.

--- garde-security-test/src/test/scala/garde/security/ModuleSpec.scala
+++ garde-security-test/src/test/scala/garde/security/ModuleSpec.scala
@@ -5,17 +5,19 @@
 package garde.security
 
 import java.io.File
-import java.util.concurrent.atomic.AtomicInteger
-import scala.concurrent.duration._
-import akka.testkit.{TestProbe, ImplicitSender, TestKit}
-import akka.actor.{ActorRef, PoisonPill, Props, ActorSystem}
-import org.scalatest.{BeforeAndAfterEach, BeforeAndAfterAll, MustMatchers, WordSpecLike}
-import akka.util.Timeout
-import scala.concurrent.Await
-import com.typesafe.config.ConfigFactory
-import scalaz._
+
+import akka.actor.{PoisonPill, ActorRef, ActorSystem, Props}
+import akka.pattern.AskSupport
 import akka.persistence.Recover
+import akka.testkit.{TestProbe, ImplicitSender, TestKit}
+import akka.util.Timeout
+import com.typesafe.config.ConfigFactory
 import org.apache.commons.io.FileUtils
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, MustMatchers, WordSpecLike}
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scalaz._
 
 object ModuleSpec {
 
@@ -30,13 +32,13 @@ val Config = ConfigFactory.parseString(s"""
 
 class ModuleSpec extends TestKit(ActorSystem("test", ModuleSpec.Config))
   with ImplicitSender
+  with AskSupport
   with WordSpecLike
   with MustMatchers
   with BeforeAndAfterAll
   with BeforeAndAfterEach {
 
-  import ActiveModule._
-  import ModuleSupervisor._
+  import garde.security.ActiveModule._
 
   implicit val timeout = Timeout(5 seconds)
 
@@ -47,7 +49,7 @@ class ModuleSpec extends TestKit(ActorSystem("test", ModuleSpec.Config))
 
   override protected def beforeAll() {
     filesToClean.foreach(FileUtils.deleteDirectory)
-    sup = system.actorOf(Props(new ModuleSupervisor()), "sup")
+    sup = system.actorOf(Props(new ModuleSupervisor()), ModuleSupervisor.ModuleSupervisorName)
   }
 
   override protected def afterAll() {
@@ -63,16 +65,9 @@ class ModuleSpec extends TestKit(ActorSystem("test", ModuleSpec.Config))
     "succeed" in {
       sup ! CreateModule("module-1", "Module1")
       expectMsg(timeout.duration, Success(ModuleCreated("module-1", 0L, "Module1")))
-      sup ! GetSupervisorState
-      expectMsg(timeout.duration, ModuleSupervisorState(Seq("module-1")))
-      val module = Await.result(system.actorSelection("/user/sup/module-1").resolveOne(), timeout.duration)
-      module ! GetState
+      val module = Await.result(system.actorSelection("/user/module-supervisor/module-1").resolveOne(), timeout.duration)
+      module ! GetModuleState
       expectMsg(timeout.duration, ModuleState("module-1", 0L, "Module1"))
-    }
-
-    "fail on duplication creation" in {
-      sup ! CreateModule("module-1", "Module1")
-      expectMsg(timeout.duration, Failure(NonEmptyList("Module module-1 already exists.")))
     }
 
     "fail with empty id" in {
@@ -107,38 +102,63 @@ class ModuleSpec extends TestKit(ActorSystem("test", ModuleSpec.Config))
   "Module change name" must {
 
     "succeed" in {
-      val module = Await.result(system.actorSelection("/user/sup/module-1").resolveOne(), timeout.duration)
+      val module = Await.result(system.actorSelection("/user/module-supervisor/module-1").resolveOne(), timeout.duration)
       module ! ChangeModuleName("module-1", 0L, "New Name")
       expectMsg(timeout.duration, Success(ModuleNameChanged("module-1", 1L, "New Name")))
     }
 
     "fail with empty name" in {
-      val module = Await.result(system.actorSelection("/user/sup/module-1").resolveOne(), timeout.duration)
+      val module = Await.result(system.actorSelection("/user/module-supervisor/module-1").resolveOne(), timeout.duration)
       module ! ChangeModuleName("module-1", 1L, "")
       expectMsg(timeout.duration, Failure(NonEmptyList(NameRequired.toString)))
     }
 
     "fail with null name" in {
-      val module = Await.result(system.actorSelection("/user/sup/module-1").resolveOne(), timeout.duration)
+      val module = Await.result(system.actorSelection("/user/module-supervisor/module-1").resolveOne(), timeout.duration)
       module ! ChangeModuleName("module-1", 1L, null)
       expectMsg(timeout.duration, Failure(NonEmptyList(NameRequired.toString)))
     }
 
     "fail with id mismatch" in {
-      val module = Await.result(system.actorSelection("/user/sup/module-1").resolveOne(), timeout.duration)
+      val module = Await.result(system.actorSelection("/user/module-supervisor/module-1").resolveOne(), timeout.duration)
       module ! ChangeModuleName("bad id", 1L, "New Name2")
       expectMsg(timeout.duration, Failure(NonEmptyList(IdMismatch.toString)))
     }
 
     "fail with incorrect version" in {
-      val module = Await.result(system.actorSelection("/user/sup/module-1").resolveOne(), timeout.duration)
+      val module = Await.result(system.actorSelection("/user/module-supervisor/module-1").resolveOne(), timeout.duration)
       module ! ChangeModuleName("module-1", 0L, "New Name2")
       expectMsg(timeout.duration, Failure(NonEmptyList(IncorrectVersion.toString)))
     }
   }
 
+  "Module locator" must {
+
+    import ModuleLocator._
+
+    "successfuly locate an instantiated module" in {
+      val m1 = Await.result(system.actorSelection("/user/module-supervisor/module-1").resolveOne(), timeout.duration)
+      val locator = system.actorOf(Props(new ModuleLocator("module-1")))
+      val located = Await.result(locator ? LocateModule, timeout.duration)
+      located must be(m1)
+    }
+
+    "successfuly locate an non-instantiated module" in {
+      val probe = TestProbe()
+      val m1 = Await.result(system.actorSelection("/user/module-supervisor/module-1").resolveOne(), timeout.duration)
+      probe watch m1
+      m1 ! PoisonPill
+      probe expectTerminated(m1, timeout.duration)
+
+      val locator = system.actorOf(Props(new ModuleLocator("module-1")))
+      val located = Await.result(locator ? LocateModule, timeout.duration).asInstanceOf[ActorRef]
+      located.path.name must be(m1.path.name)
+    }
+  }
 
   "Module recovery" must {
+
+    import ModuleSupervisor._
 
     "succeed" in {
       val probe = TestProbe()
@@ -146,19 +166,10 @@ class ModuleSpec extends TestKit(ActorSystem("test", ModuleSpec.Config))
       sup ! PoisonPill
       probe expectTerminated(sup, timeout.duration)
 
-      sup = system.actorOf(Props(new ModuleSupervisor()), "sup")
-      sup ! Recover
-      sup ! GetSupervisorState
-      expectMsg(timeout.duration, ModuleSupervisorState(Seq("module-1", "module-2", "module-3")))
-
-      Thread.sleep(2000)
-      val m1 = Await.result(system.actorSelection("/user/sup/module-1").resolveOne(), timeout.duration)
-      m1 ! GetState
+      sup = system.actorOf(Props(new ModuleSupervisor()), ModuleSupervisorName)
+      val m = Await.result(sup ? RecoverModule("module-1"), timeout.duration).asInstanceOf[ActorRef]
+      m ! GetModuleState
       expectMsg(timeout.duration, ModuleState("module-1", 1L, "New Name"))
-
-//      val m2 = Await.result(system.actorSelection("/user/sup/module-2").resolveOne(), timeout.duration)
-//      m2 ! GetState
-//      expectMsg(timeout.duration, ModuleState("module-2", 0L, "Module2"))
     }
   }
 }
--- garde-security/src/main/scala/garde/security/Module.scala
+++ garde-security/src/main/scala/garde/security/Module.scala
@@ -7,26 +7,11 @@ package garde.security
 import akka.actor._
 import akka.actor.OneForOneStrategy
 import akka.actor.SupervisorStrategy._
-import akka.persistence.{Recover, PersistentActor, SnapshotOffer}
+import akka.pattern.PipeToSupport
+import akka.persistence.{RecoveryCompleted, Recover, PersistentActor, SnapshotOffer}
 import akka.util.Timeout
-import garde.security.ModuleSupervisor.CreationException
 import scalaz._
 import Scalaz._
-
-/**
- * This is the current state of any given module.
- * @param id String the module id
- * @param version Long the current version
- * @param name String the name of the module.
- */
-sealed case class ModuleState(id: String, version: Long, name: String)
-
-/**
- * This is the current state of the module supervisor, which must have a collection of all existing modules
- * in order to instantiate the module persistent actors.
- * @param modules Seq[String] the collection of module ids.
- */
-sealed case class ModuleSupervisorState(var modules: Seq[String])
 
 /**
  * Persistent actor representing an active module.
@@ -35,29 +20,20 @@ class ActiveModule(id: String)(implicit val timeout: Timeout) extends Persistent
 
   import CommonValidations._
   import ActiveModule._
-  import ModuleSupervisor._
 
   override def persistenceId = id
   var state: ModuleState = _
 
   val receiveCommand: Receive = {
-
-    case cmd @ CreateModule(id, name)                       =>
-      try {
-        validateCreate(cmd) match {
-          case Success(v)     =>
-            persist(ModuleCreated(v.id, 0L, v.name)) { event =>
-              state = ModuleState(event.id, event.version, event.name)
-              sender ! event
-            }
-          case f @ Failure(v) =>
-            sender ! CreationException(id, f)
-            context stop self
-        }
-      }
-      catch {
-        case e: Exception =>
-          //sender ! CreationException(id, e.getMessage.failureNel)
+    case cmd @ CreateModule(id, name)                      =>
+      validateCreate(cmd) match {
+        case Success(v)     =>
+          persist(ModuleCreated(v.id, 0L, v.name)) { event =>
+            state = ModuleState(event.id, event.version, event.name)
+            sender ! event
+          }
+        case f @ Failure(v) =>
+          sender ! CreationException(id, f)
           context stop self
       }
 
@@ -73,12 +49,14 @@ class ActiveModule(id: String)(implicit val timeout: Timeout) extends Persistent
 
     case SaveModuleSnapshot                                => saveSnapshot(state)
 
-    case GetState                                          => sender ! state
+    case GetModuleState                                    => sender ! state
+
+    case RecoveryCompleted                                 => sender ! ModuleRecoveryComplete(recoveryFinished)
   }
 
   val receiveRecover: Receive = {
     case evt: ModuleCreated                      => state = ModuleState(evt.id, evt.version, evt.name)
-    case evt: ModuleNameChanged                  => state = state.copy(name = evt.name)
+    case evt: ModuleNameChanged                  => state = state.copy(name = evt.name, version = evt.version)
     case SnapshotOffer(_, snapshot: ModuleState) => state = snapshot
   }
 
@@ -102,6 +80,8 @@ class ActiveModule(id: String)(implicit val timeout: Timeout) extends Persistent
  */
 object ActiveModule {
 
+  sealed case class ModuleState(id: String, version: Long, name: String)
+
   case object IncorrectVersion extends ValidationKey
   case object IdRequired extends ValidationKey
   case object IdMismatch extends ValidationKey
@@ -118,62 +98,40 @@ object ActiveModule {
   final case class ModuleCreated(id: String, version: Long, name: String)
   final case class ModuleNameChanged(id: String, version: Long, name: String)
 
+  sealed case class CreationException(moduleId: String, validation: Failure[NonEmptyList[String], CreateModule])
   case object SaveModuleSnapshot
-  case object GetState
+  case object GetModuleState
+  case class ModuleRecoveryComplete(completed: Boolean)
 }
 
 /**
  * An actor as a factory for modules and for supervision.
  */
-class ModuleSupervisor(implicit val timeout: Timeout) extends PersistentActor {
+class ModuleSupervisor(implicit val timeout: Timeout) extends Actor {
 
-  import CommonValidations._
   import ActiveModule._
   import ModuleSupervisor._
-
-  override def persistenceId = PersistenceId
-  var state = ModuleSupervisorState(Nil)
+  import CommonValidations._
 
   override val supervisorStrategy =
     OneForOneStrategy() {
       case _: Exception => Restart
     }
 
-  def receiveCommand = {
-    case cmd @ CreateModule(id, name)           =>
+  def receive = {
+    case cmd @ CreateModule(id, name)  =>
       val v = checkString(id, IdRequired)
       if (v.isSuccess) {
-        state.modules.find(_ == v.toOption.get) match {
-          case Some(m) => sender ! s"Module ${cmd.id} already exists.".failureNel
-          case None    =>
-            try {
-              val client = sender
-              context actorOf(Props(new ModuleCreationSaga(client, cmd, context.actorOf(Props(new ActiveModule(cmd.id)), cmd.id))),
-                s"module-creation-saga-$id")
-            }
-            catch {
-              case e: Exception => sender ! e.getMessage.failureNel
-            }
-        }
+        val client = sender
+        context actorOf(Props(new ModuleCreationSaga(client, cmd, context.actorOf(Props(new ActiveModule(cmd.id)), cmd.id))),
+          s"module-creation-saga-$id")
       }
       else sender ! v.toValidationNel
 
-    case evt @ ModuleCreated(id, version, name) =>
-      persist(ModuleCreationReceived(id)) { event =>
-        state.modules  = state.modules :+ event.id
-      }
-
-    case SaveModuleSupervisorSnapshot           => saveSnapshot(state)
-
-    case GetSupervisorState                     => sender ! state
-  }
-
-  val receiveRecover: Receive = {
-    case evt: ModuleCreationReceived                       =>
-      state.modules = state.modules :+ evt.id
-      context.actorOf(Props(new ActiveModule(evt.id)), evt.id) ! Recover
-
-    case SnapshotOffer(_, snapshot: ModuleSupervisorState) => state = snapshot
+    case msg @ RecoverModule(moduleId) =>
+      val m = context.actorOf(Props(new ActiveModule(moduleId)), moduleId)
+      m ! Recover
+      sender ! m
   }
 }
 
@@ -181,13 +139,8 @@ class ModuleSupervisor(implicit val timeout: Timeout) extends PersistentActor {
  * Companion object for ModuleSupervisor.
  */
 object ModuleSupervisor {
-  import ActiveModule._
-
-  final val PersistenceId = "module-supervisor-persistence-id"
-  sealed case class CreationException(moduleId: String, validation: Failure[NonEmptyList[String], CreateModule])
-  final case class ModuleCreationReceived(id: String)
-  case object SaveModuleSupervisorSnapshot
-  case object GetSupervisorState
+  final val ModuleSupervisorName = "module-supervisor"
+  sealed case class RecoverModule(moduleId: String)
 }
 
 /**
@@ -217,7 +170,6 @@ class ModuleCreationSaga(client: ActorRef, cmd: ActiveModule.CreateModule, actor
 
   def awaitCreation: Receive = {
     case evt @ ModuleCreated(id, version, name) =>
-      context.parent ! evt
       context.system.eventStream.publish(evt)
       client ! evt.success
       context stop self
@@ -231,4 +183,65 @@ class ModuleCreationSaga(client: ActorRef, cmd: ActiveModule.CreateModule, actor
       client ! s"CreateModule timed out after ${timeout.duration.toSeconds} seconds for module:${cmd.id}".failureNel
       context stop self
   }
+}
+
+class ModuleLocator(moduleId: String)(implicit val timeout: Timeout) extends Actor {
+
+  import ModuleLocator._
+  import ActiveModule._
+  import ModuleSupervisor._
+
+  context.setReceiveTimeout(timeout.duration)
+
+  var client: ActorRef = _
+
+  def receive = {
+    case LocateModule =>
+      context become awaitLookup
+      client = sender
+      context.actorSelection(s"/user/$ModuleSupervisorName/module-1") ! Identify(moduleId)
+  }
+
+  def awaitLookup: Receive = {
+    case ActorIdentity(identityId, Some(ref)) =>
+      client ! ref
+      context stop self
+
+    case ActorIdentity(identityId, None)      =>
+      context become awaitInstantiation
+      context.actorSelection(s"/user/$ModuleSupervisorName") ! RecoverModule(moduleId)
+
+    case ReceiveTimeout                       =>
+      client ! s"Unable to establish identity for module $moduleId."
+      context stop self
+  }
+
+  def awaitInstantiation: Receive = {
+    case m: ActorRef    =>
+      context become awaitRecovery
+      m ! RecoveryCompleted
+
+    case ReceiveTimeout =>
+      client ! s"Unable to instantiate module $moduleId."
+      context stop self
+  }
+
+  def awaitRecovery: Receive = {
+    case msg @ ModuleRecoveryComplete(completed) =>
+      if (completed) {
+        val module = sender
+        client ! module
+        context stop self
+      }
+      else
+        sender ! RecoveryCompleted
+
+    case ReceiveTimeout =>
+      client ! s"Unable to instantiate module $moduleId."
+      context stop self
+  }
+}
+
+object ModuleLocator {
+  case object LocateModule
 }
